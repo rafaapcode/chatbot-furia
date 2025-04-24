bc0da763ad41be150c1596bae1af7a90490836bc
rafaapcode <rafaap2003@gmail.com>
2025-04-24 07:55:57 -0300

    feat: creating the chat page

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -1,8 +1,9 @@
 "use client";
 
 import SearchComponent from "@/components/searchComponent";
-import { useSearchParams } from "next/navigation";
-import { ChangeEvent, useEffect, useState } from "react";
+import { ChevronLeft } from "lucide-react";
+import { useRouter, useSearchParams } from "next/navigation";
+import { ChangeEvent, useEffect, useRef, useState } from "react";
 
 interface Message {
   content: string;
@@ -14,6 +15,8 @@ export default function SearchPage() {
   const [input, setInput] = useState("");
   const [isLoading, setIsLoading] = useState(false);
   const params = useSearchParams();
+  const messagesEndRef = useRef<HTMLDivElement>(null);
+  const router = useRouter();
 
   useEffect(() => {
     const searchValue = params.get("search");
@@ -23,9 +26,12 @@ export default function SearchPage() {
         { content: query, isUser: true },
         { content: `Pesquisando sobre: ${query}...`, isUser: false },
       ]);
-      // Aqui você adicionaria a chamada à API para responder à consulta
     }
   }, [params]);
+
+  useEffect(() => {
+    messagesEndRef.current?.scrollIntoView({ behavior: 'smooth' });
+  }, [messages, isLoading]);
 
   const handleSubmit = async () => {
     if (!input.trim()) return;
@@ -53,14 +59,19 @@ export default function SearchPage() {
     }
   };
 
+  const onClick = () => router.push("/");
+
   return (
     <main className="h-screen p-2">
-      <div className="border h-full flex flex-col border-neutral-700 rounded-xl bg-neutral-900">
+      <button onClick={onClick} className="bg-neutral-800 p-1 mb-2 rounded-lg hover:bg-neutral-700 transition-all duration-200">
+        <ChevronLeft size={16}/>
+      </button>
+      <div className="border h-[95%] flex flex-col border-neutral-700 rounded-xl bg-neutral-900">
         <div className="flex-1 overflow-y-auto p-4">
           {messages.map((message, i) => (
             <div
               key={i}
-              className={`mb-4 p-3 rounded-lg w-fit max-w-[80%] overflow-y-auto ${
+              className={`mb-4 p-3 rounded-lg w-fit max-w-[80%] break-words whitespace-normal overflow-x-hidden ${
                 message.isUser
                   ? "ml-auto bg-zinc-800 text-white"
                   : "bg-zinc-600 text-white"
@@ -70,10 +81,11 @@ export default function SearchPage() {
             </div>
           ))}
           {isLoading && (
-            <div className="bg-neutral-800 text-white p-3 rounded-lg w-fit max-w-[80%] animate-pulse">
+            <div className="bg-neutral-800 text-white p-3 rounded-lg w-fit max-w-[80%] break-words whitespace-normal overflow-x-hidden animate-pulse">
               Pensando...
             </div>
           )}
+          <div ref={messagesEndRef} />
         </div>
 
         <div className="w-full py-2">
