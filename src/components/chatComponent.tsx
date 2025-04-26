5f823b3422ab415cee3290f2b539deb586fab090
rafaapcode <rafaap2003@gmail.com>
2025-04-26 12:56:55 -0300

    feat: fixing the style of the chat component

--- src/components/chatComponent.tsx
+++ src/components/chatComponent.tsx
@@ -72,7 +72,7 @@ function ChatComponent({
   }, [messages, isLoading]);
 
   return (
-    <div className="flex-1 overflow-y-auto p-4">
+    <div className="flex-1 overflow-y-auto p-4 w-[90%] mx-auto">
       {messages.map((message, i) => (
         <div
           key={i}
